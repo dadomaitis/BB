d6100d5b82e8f0661e106fa5393437df40b47541
dadomaitis <domasadomaitis@gmail.com>
2023-05-01 19:46:49 +0300

    Tabs 95% done js

---
+++ script.js
@@ -0,0 +1,15 @@
+function openTab(evt, TabFunction) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Parodo norimą tab pagal id
+    document.getElementById(TabFunction).style.display = "flex";
+    evt.currentTarget.className += " active";
+}
